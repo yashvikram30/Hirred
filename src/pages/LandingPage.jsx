a00ba624df3fca257176b766a4aef2a9430ff53e
yash <yashvikram8250@gmail.com>
2025-01-22 12:18:12 +0530

    created the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,127 @@
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import companies from "../data/companies.json";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import faqs from "../data/faq.json";
 
 const LandingPage = () => {
   return (
-    <div>
-      landingpage
-    </div>
-  )
-}
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20 mx-20">
+      <section className="text-center">
+        <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4 ">
+          Find Your Dream Job{" "}
+          <span className="flex justify-center items-center gap-2 sm:gap-6">
+            {" "}
+            and get{" "}
+            <img
+              src="../../public/logo.png"
+              alt="Hirred Logo"
+              className="h-14 sm:h-24 lg:h-32"
+            />
+          </span>
+        </h1>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
+      </section>
 
-export default LandingPage
+      <div className="flex gap-6 justify-center">
+        <Link to="/jobs">
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to="/post-job">
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
+        </Link>
+      </div>
+
+      <div>
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          opts={{
+            align: "start",
+          }}
+          className="w-full py-10"
+        >
+          <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+            {companies.map(({ name, id, path }) => {
+              return (
+                <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                  <img
+                    src={path}
+                    alt={name}
+                    className="h-9 sm:h-14 object-contain"
+                  />
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+        </Carousel>
+      </div>
+
+      <img
+        src="../../public/banner.jpeg"
+        className="w-full"
+        alt="Banner Image"
+      />
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For job seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Search and apply for jobs, track applications, and more.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Post jobs,manage application, and find the best candidates.</p>
+          </CardContent>
+        </Card>
+      </section>
+
+      <Accordion type="single" collapsible>
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem key={index} value={`items-${index + 1}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </main>
+  );
+};
+
+export default LandingPage;
